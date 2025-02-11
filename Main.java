6d321858ba6742b75ab328553336547607e71f63
Ravi Sanker <ravigadu.19@gmail.com>
2025-02-11 16:42:43 +0530

    add constructor

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
+    public static int x = 123;
+    public String myName = "Ravi";
+
+    public static int addNum(int a, int b) {
+        return a + b;
+    }
+
+    Main(int a, int b, int c) {
+        this.x = a;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Main myObj = new Main(19, 05, 1997);
+        System.out.println(myObj.addNum(19, 05));
+        System.out.println(x);
     }
 }
