cad1fc3c23cd7c8825d2a01bfb868624b25bc3cd
Ravi Sanker <ravigadu.19@gmail.com>
2025-02-11 17:03:46 +0530

    Say Author function added

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ public class Main {
     public String myName = "Ravi";
 
     public static int addNum(int a, int b) {
-        return a*17663;
+        return a * 17663;
     }
 
-    public static void sayHi(){
+    public static void sayHi() {
         System.out.println("Hello mr.v");
     }
+
     Main(int a, int b, int c) {
         this.x = a;
     }
@@ -21,7 +22,7 @@ public class Main {
         Main myObj = new Main(19, 05, 1997);
         System.out.println(myObj.addNum(19, 05));
         System.out.println("Hello, World!");
-        // I have some change in the git epo
+        // I have some change
         sayHi();
 
     }
