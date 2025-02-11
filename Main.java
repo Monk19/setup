56a399b86f46fc365467afdf2a4d378cab87e910
Ravi Shan <ravigadu.19@gmail.com>
2025-02-11 17:08:14 +0530

    some changes

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
 public class Main {
-    public static int x = 123;
-    public String myName = "Ravi";
-
+  
     public static int addNum(int a, int b) {
-        return a * 17663;
+        return b * 17663;
     }
 
-    public static void sayHi() {
-        System.out.println("Hello mr.v");
-    }
 
     Main(int a, int b, int c) {
         this.x = a;
@@ -20,10 +15,10 @@ public class Main {
 
     public static void main(String[] args) {
         Main myObj = new Main(19, 05, 1997);
+        // I have some change
         System.out.println(myObj.addNum(19, 05));
         System.out.println("Hello, World!");
-        // I have some change
-        sayHi();
+        myObj.whosAuthor();
 
     }
 }
